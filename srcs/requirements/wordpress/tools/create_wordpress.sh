d887ac1c1c3a8da0edd113bc57bc64ab0d8b686d
NargusS <https://NargusS@github.com>
2022-11-15 10:41:26 +0100

    Modify wordpress and mariadb

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -11,12 +11,6 @@ else
 	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
 	mv ./www.conf /etc/php/7.3/fpm/pool.d/
 
-	# cd /var/www/html/wordpress
-	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	# mv wp-config-sample.php wp-config.php
 	mv /var/www/wp-config.php /var/www/html/wordpress;
 fi
 
